9e9ee47b9c3c3ed021c398379e6127b0afa4f2b5
Darragh Ferry <darraghferry@hotmail.com>
2020-01-06 18:03:08 +0000

    axios call in App.js not working properly

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { connect } from "react-redux";
+import axiosWithAuth from "./axios/helper";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("https://kids-fly-backend.herokuapp.com/login", {
+        email: "Sydnee_Kuhlman@yahoo.com",
+        password: "password"
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +40,10 @@ function App() {
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    trips: state.trips
+  };
+}
+
+export default connect(mapStateToProps, {})(App);
